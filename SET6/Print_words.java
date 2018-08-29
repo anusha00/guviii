e910bd0f23dec37a0ede2791ba9a8d83063ada08
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-08-29 10:53:56 +0530

    Create Print_words.java

---
+++ SET6/Print_words.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=s.nextInt();
+		switch(n)
+		{
+			case 1:System.out.print("One");break;
+			case 2:System.out.print("Two");break;
+			case 3:System.out.print("Three");break;
+			case 4:System.out.print("Four");break;
+			case 5:System.out.print("Five");break;
+			case 6:System.out.print("Six");break;
+			case 7:System.out.print("Seven");break;
+			case 8:System.out.print("Eight");break;
+			case 9:System.out.print("Nine");break;
+			case 10:System.out.print("Ten");break;
+			default:System.out.println("Invalid");
+			
+		}
+	}
+}
