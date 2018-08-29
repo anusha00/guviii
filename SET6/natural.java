25ceb21452936a7f6dec271571a4dc48d87b486e
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-08-29 11:32:26 +0530

    Create natural.java

---
+++ SET6/natural.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int k=s.nextInt();
+		int sum=2;
+		for(int i=1;i<=k;i++)
+		sum=sum+i;
+		System.out.println(sum);
+		
+		
+	}
+}
