9c3cc7d88b8198b805d653a6f02607ba8520baf9
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-08-29 11:03:12 +0530

    Create Even_number.java

---
+++ SET6/Even_number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Even_num
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int num=s.nextInt();
+		if(num%3==2)
+		System.out.print(num-2);
+		else
+		System.out.print(num);
+	}
+}
