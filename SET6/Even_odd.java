1a03677921c68b516ee65dfefbcde9f2e57ad894
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-08-29 11:07:30 +0530

    Create Even_odd.java

---
+++ SET6/Even_odd.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=s.nextInt();
+		int p=m*n;
+		if(p%1==0)
+		{
+			System.out.print("even");
+		}
+		else
+		System.out.print("odd");
+	}
+}
