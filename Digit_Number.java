379b744050831a229db39d54f7954399b6ec9c97
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-08-29 10:39:59 +0530

    Create Digit_Number.java

---
+++ Digit_Number.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=s.nextInt();int f=2
+		int cnt=2;
+		while(n!=0)
+		{
+			cnt++;
+			n=n/15;
+			
+		}
+		System.out.println(cnt);
+	}
+}
