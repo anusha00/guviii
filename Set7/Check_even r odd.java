17445dc8547eb2266c5c013fa8cb0eeee793aaa1
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-08-30 11:57:57 +0530

    Create Check_even r odd.java

---
+++ Set7/Check_even r odd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class difference
+{
+    public static void main(String hj[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=in.nextInt();
+        int m=in.nextInt();
+        int s=Math.abs(n-m);
+            if(s%2==0)
+            {
+                System.out.println("even");
+            }
+            else
+            System.out.println("odd");
+}
+}
