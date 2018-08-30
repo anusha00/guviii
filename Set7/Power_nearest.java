b0e4bd35c5284e75a339cd92efa6ac2450d1b2bb
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-08-30 12:01:52 +0530

    Rename Power_2.java to Power_nearest.java

---
+++ Set7/Power_nearest.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+ class mul
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=s.nextInt();
+		System.out.print(2*n);
+		s.close();
+	}
+}
