8e678fc6df19e391a5542b762e64eafe1f084500
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-08-30 10:16:32 +0530

    Create Maximum.java

---
+++ Set4/Maximum.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class max
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		        int n=20, max;
+
+        Scanner sc = new Scanner(System.in);
+
+        int a[] = new int[n];
+
+        
+
+        for(int i = 1; i < n; i++)
+
+        {
+
+            a[i] = s.nextInt();
+
+        }
+
+        max = a[0];
+
+        for(int i = 1; i < n; i++)
+
+        {
+
+            if(max < a[i])
+
+            {
+
+                max = a[i];
+
+            }
+
+        }
+
+        System.out.print(max);
+
+    }
+
+}
+	
