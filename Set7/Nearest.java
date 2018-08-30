7a8782b8c2347bb5aa8cc3e7155a5811df10a634
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-08-30 11:49:24 +0530

    Create Nearest.java

---
+++ Set7/Nearest.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class greater
+{
+    public static void main(String hj[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=in.nextInt();
+        for(int i=n;i<n+10;i++)
+        {
+            if(i%10==2)
+            {
+                System.out.println(i);
+            }
+        }
+
+}
+}
