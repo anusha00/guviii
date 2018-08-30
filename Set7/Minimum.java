326ba41fe98af1c6834709ea272c30d40bf28558
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-08-30 11:32:59 +0530

    Create Minimum.java

---
+++ Set7/Minimum.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+ class minium
+ {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n;
+		   int a[]=new int[20];
+           for(int i=0;i<20;i++)
+        {
+        	   a[i]=s.nextInt();
+          }
+                 Arrays.sort(a);
+          System.out.println(a[0]);
+        }
+}
