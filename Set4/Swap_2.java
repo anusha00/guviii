e6b74d0db84ddb194a7dbc34c82629bcdedd67f0
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-08-30 10:07:47 +0530

    Create Swap_2.java

---
+++ Set4/Swap_2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+ class swap2
+{
+
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		int a=scanner.nextInt();
+		int b=scanner.nextInt();
+		int temp;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.print(" " +a+" "+b);
+		
+
+	}
+
+}
