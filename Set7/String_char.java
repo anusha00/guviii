efd56acc58feb2d69ab69fc2cf14fd93bb7d0be7
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-08-30 10:28:04 +0530

    Create String_char.java

---
+++ Set7/String_char.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+class kchar
+{
+     public static void main(String aa[]){
+     String d;
+     int n;
+     Scanner sc=new Scanner(System.in);
+     d=scanner.next();
+     n=scanner.nextInt();
+     char a[]=d.toCharArray();
+     for(int i=0;i<n;i++){
+        System.out.print("%c",a[i]);
+     }
+}
+}
