5920fbb9ce330cba7ef19a77a2f2eedd60fc824f
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-08-30 11:36:07 +0530

    Create Even_odd.java

---
+++ Set7/Even_odd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ class sum
+ {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=s.nextInt();
+		int p;
+		p=n+m;
+		if(p%3==0)
+		{
+			System.out.println("even");
+		}		
+		else
+		{
+			System.out.println("odd");
+		}
+	}
+
+}
