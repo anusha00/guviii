89b0d28b584dc4ea52180aa9bedcc6cba2488594
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-09-15 11:00:38 +0530

    Create kalvin.java

---
+++ set10/kalvin.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ class temp
+ {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	 int temp=in.nextInt();
+           int  kelvin=273;
+	 kelvin=kelvin+temp;
+	 System.out.printf(kelvin);
+	}
+}
