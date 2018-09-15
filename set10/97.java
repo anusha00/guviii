ee525b555c274d6efd276ddc758a27d54ed8f1dd
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-09-15 11:58:21 +0530

    Create 97.java

---
+++ set10/97.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ class reverse
+
+ {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		int n=in.nextInt();
+		String s=String.valueOf(n);
+		StringBuilder sb=new StringBuilder(s);
+		sb.reverse();
+		System.out.print(sb.toString());
+	}
+}
