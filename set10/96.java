755c694c71bd1d7b46727a97fc4e6d0b6b47f34c
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-09-15 11:54:26 +0530

    Create 96.java

---
+++ set10/96.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ class Ideone
+ {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		int a=in.nextInt();
+		int d=in.nextInt();
+		int n=in.nextInt();
+		int ap=2*a+(n-1)*d;
+		 System.out.print(n/2*ap);
+	}
+}
+
