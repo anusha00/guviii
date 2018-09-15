3b0a1de8f7deead875a2b564ab9520f222a718ec
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-09-15 11:40:00 +0530

    Create 95.java

---
+++ set10/95.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ class floor
+ {
+
+	public static void main(String[] args)
+	{
+              Scanner input=new Scanner(System.in);
+              int p=in.nextInt();
+              int t=in.nextInt();
+              int r=in.nextInt();
+              int si=(p*t*r)/100;
+              System.out.print(si);
+	}
+
+}
