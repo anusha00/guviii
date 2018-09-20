a45e3e40f084fdddfdb2f642ccacb681339078d6
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-09-20 10:11:59 +0530

    Create n>k.java

---
+++ set11/n>k.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class print
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in=new Scanner(System.in);
+		int in=sc.nextInt();
+		int k=sc.nextInt();
+		int res=1;
+		for(int i=1;i<=k;i++)
+		{
+			res=res*n;
+		}
+		System.out.print(res);
+	}
+}
