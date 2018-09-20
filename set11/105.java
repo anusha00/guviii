be6101d5e702f834c33f4db4824af79c4a784729
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-09-20 10:26:09 +0530

    Create 105.java

---
+++ set11/105.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class join
+{
+
+	public static void main(String[] args) 
+	{
+Scanner sc=new Scanner(System.in);
+String sc=in.nextLine();
+String sc=str.replaceAll(" ","");
+System.out.print(s);
+	}
+
+}
