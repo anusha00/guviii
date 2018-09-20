8ecbc1233ca39c0f8c81eecbe899111e1921e96a
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-09-20 10:48:34 +0530

    Create 108.java

---
+++ set11/108.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Set108 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int sc=in.nextInt();
+		int sc=in.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=in.nextInt();
+		}
+		Arrays.sort(a);
+		System.out.print(a[k-1]);
+		}
+}
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
