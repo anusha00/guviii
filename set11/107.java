7d5add8f33119e7db0ab352067443caf6e0512a7
anusha00 <41770751+anusha00@users.noreply.github.com>
2018-09-20 10:39:09 +0530

    Create 107.java

---
+++ set11/107.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String strn = "yes";
+        boolean numeric = true;
+
+        try {
+                Double num = Double.parseDouble(string);
+            }    catch (NumberFormatException e)
+        {
+            numeric = false;
+        }
+
+        if(numeric)
+            System.out.printf(string + " ");
+        else
+            System.out.printf(string + " ");
+	}
+}
